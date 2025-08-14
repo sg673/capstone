4c2bcbad17e47c45f447277b3c6e1e56c50bde78
sg673 <sg673@student.le.ac.uk>
2025-08-14 14:35:25 +0100

    fix: changed how data folder is accessed

--- etl_process/src/utils/get_data.py
+++ etl_process/src/utils/get_data.py
@@ -1,15 +1,17 @@
 import pandas as pd
+from pathlib import Path
 
 
 def get_raw_file(fileName: str) -> pd.DataFrame:
     """
-    Extracts a file from the raw folder and converts it into a DataFrame.
+        Extracts a CSV file from the raw data folder and loads it into a pandas DataFrame.
     """
+    data_path = Path(__file__).parent.parent.parent / "data" / "raw" / fileName
     try:
-        return pd.read_csv(f"../../data/raw/{fileName}")
+        return pd.read_csv(data_path)
     except FileNotFoundError:
         raise FileNotFoundError(
-            f"File {fileName} not found in raw data folder"
+            f"File {fileName} not found in {data_path}"
         )
     except Exception as e:
         raise Exception(f"Error reading file {fileName}: {str(e)}")
