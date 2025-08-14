b88ebc9bc7acf354035d0e1494c6d72fb4d1e2a6
sg673 <sg673@student.le.ac.uk>
2025-08-14 16:19:19 +0100

    fix: added test config so logs are not generated during tests

---
+++ etl_process/tests/unit_tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+import logging
+
+
+@pytest.fixture(autouse=True)
+def disable_logging():
+    """Disable logging during tests"""
+    logging.disable(logging.CRITICAL)
+    yield
+    logging.disable(logging.NOTSET)
