7882effe10f8162bc8bd73f3e2bfbf43848c1377
sg673 <sg673@student.le.ac.uk>
2025-08-14 15:00:57 +0100

    style: files now conform to PEP 8 guidelines

--- etl_process/src/utils/get_data.py
+++ etl_process/src/utils/get_data.py
@@ -4,26 +4,32 @@ from src.utils.logging_utils import setup_logger, log_extract_success
 import logging
 import timeit
 
-logger = setup_logger(__name__, "extract_data.log",level=logging.DEBUG)
+logger = setup_logger(__name__, "extract_data.log", level=logging.DEBUG)
 
 
 def get_raw_file(fileName: str) -> pd.DataFrame:
     """
-        Extracts a CSV file from the raw data folder and loads it into a pandas DataFrame.
+        Extracts a CSV file from the raw data folder and loads it into a pandas
+        DataFrame.
+
         Args:
-            fileName (str): The name of the CSV file to be loaded from the '../../data/raw/' directory.
+            fileName (str): The name of the CSV file to be loaded from the
+            '../../data/raw/' directory.
         Returns:
-            pd.DataFrame: The contents of the CSV file as a pandas DataFrame.
+            pd.DataFrame: The contents of the CSV file as a pandas
+            DataFrame.
         Raises:
-            FileNotFoundError: If the specified file does not exist in the raw data folder.
-            Exception: If there is an error reading the file (e.g., parsing error, permission error).
+            FileNotFoundError: If the specified file does not exist in the
+            raw data folder.
+            Exception: If there is an error reading the file (e.g., parsing
+            error, permission error).
         Example:
             df = get_raw_file("data.csv")
     """
 
     start_time = timeit.default_timer()
     data_path = Path(__file__).parent.parent.parent / "data" / "raw" / fileName
-    
+
     try:
         df = pd.read_csv(data_path)
         extract_file_execution_time = timeit.default_timer() - start_time
