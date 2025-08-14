3d5ff882993c2dad110cc94f22c6caded48a6eb1
sg673 <sg673@student.le.ac.uk>
2025-08-14 14:35:43 +0100

    style: added docstring

--- etl_process/src/utils/get_data.py
+++ etl_process/src/utils/get_data.py
@@ -5,6 +5,15 @@ from pathlib import Path
 def get_raw_file(fileName: str) -> pd.DataFrame:
     """
         Extracts a CSV file from the raw data folder and loads it into a pandas DataFrame.
+        Args:
+            fileName (str): The name of the CSV file to be loaded from the '../../data/raw/' directory.
+        Returns:
+            pd.DataFrame: The contents of the CSV file as a pandas DataFrame.
+        Raises:
+            FileNotFoundError: If the specified file does not exist in the raw data folder.
+            Exception: If there is an error reading the file (e.g., parsing error, permission error).
+        Example:
+            df = get_raw_file("data.csv")
     """
     data_path = Path(__file__).parent.parent.parent / "data" / "raw" / fileName
     try:
