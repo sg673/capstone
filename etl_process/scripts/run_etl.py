dfdb132d1efa665ebbb26431d8921b76ce87ded7
sg673 <sg673@student.le.ac.uk>
2025-08-15 12:24:09 +0100

    refactor: added logging to run_etl

--- etl_process/scripts/run_etl.py
+++ etl_process/scripts/run_etl.py
@@ -2,16 +2,26 @@ import os
 import sys
 from config.env_config import setup_env
 from src.extract.extract import extract_main
+from src.utils.logging_utils import setup_logger
+
+# Use LOG_BASE_PATH if set (for testing), otherwise use default
+log_base_path = os.getenv("LOG_BASE_PATH")
+logger = setup_logger(
+    "etl_pipeline", "etl_pipeline.log", base_path=log_base_path
+)
 
 
 def main():
     # Get the argument from the run_etl command and set up the environment
     setup_env(sys.argv)
-    print(
-        f"ETL pipeline run successfully in "
+    logger.info(
+        f"ETL pipeline started successfully in "
         f"{os.getenv('ENV', 'error')} environment!"
     )
-    extract_main()
+
+    logger.info("Started Extraction Phase")
+    extracted_data = extract_main()
+    logger.info("Extraction Phase Completed")
 
 
 if __name__ == "__main__":
