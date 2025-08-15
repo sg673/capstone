429cea008306d2cf92b106413a26843d43dae740
sg673 <sg673@student.le.ac.uk>
2025-08-15 12:12:15 +0100

    feat: added delay data to extract_main

--- etl_process/src/extract/extract.py
+++ etl_process/src/extract/extract.py
@@ -1,3 +1,4 @@
+from src.extract.get_delay_data import extract_delay_data
 from src.extract.get_airports import extract_airport_locations
 
 
@@ -6,3 +7,4 @@ def extract_main() -> None:
         Runs the extraction phase
     """
     extract_airport_locations()
+    extract_delay_data()
