55863c41686ece8342afbfb4cb690bdca29bacc1
sg673 <sg673@student.le.ac.uk>
2025-08-18 10:35:50 +0100

    feat: null values are now dealt with based on column

--- etl_process/src/transform/clean_delay.py
+++ etl_process/src/transform/clean_delay.py
@@ -8,9 +8,19 @@ logger = setup_logger(__name__, "extract_data.log", level=logging.DEBUG)
 def clean_delay_data(data: pd.DataFrame) -> pd.DataFrame:
     """
     """
+
     duplicated_rows = data.duplicated()
     logger.info(f"Detected {duplicated_rows.sum()} duplicate rows")
     clean_data = data[~duplicated_rows]
 
+    CRITICAL_COLUMNS = ['year',
+                        'month',
+                        'carrier',
+                        'carrier_name',
+                        'airport',
+                        'airport_name']
+    clean_data = clean_data.dropna(subset=CRITICAL_COLUMNS)
+    clean_data = clean_data.fillna(0)
 
+    
     return clean_data
