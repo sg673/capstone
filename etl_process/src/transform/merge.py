6c3ef5e9695b097661c24490f2142722b173521a
sg673 <sg673@student.le.ac.uk>
2025-08-18 16:42:26 +0100

    style: added docstring

--- etl_process/src/transform/merge.py
+++ etl_process/src/transform/merge.py
@@ -4,6 +4,19 @@ import pandas as pd
 
 def merge_main(airport_df: pd.DataFrame,
                delay_df: pd.DataFrame) -> pd.DataFrame:
+    """
+        Merge airport and delay dataframes and extract state information.
+
+        Args:
+            airport_df: DataFrame containing airport
+            information with 'iata' codes
+            delay_df: DataFrame containing flight
+            delay data with 'airport' codes
+
+        Returns:
+            Merged DataFrame with state column added and
+            unnecessary columns removed
+    """
     merged_df = delay_df.merge(airport_df,
                                left_on='airport',
                                right_on='iata',
