d6e6c78f4314adb627e0050c72ba8fc9c9af33ca
sg673 <sg673@student.le.ac.uk>
2025-08-18 16:36:15 +0100

    feat: created merge file

---
+++ etl_process/src/transform/merge.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+
+
+def merge_main(airport_df: pd.DataFrame,
+               delay_df: pd.DataFrame) -> pd.DataFrame:
+    merged_df = delay_df.merge(airport_df,
+                               left_on='airport',
+                               right_on='iata',
+                               how='left')
+    # ~5000 unmatched codes
+    merged_df.dropna(inplace=True)
+    merged_df['state'] = merged_df['airport_name'] \
+        .str.split(', ') \
+        .str[1].str.split(':').str[0]
+    merged_df = merged_df.drop(['Unnamed: 0_x',
+                                'arr_del15',
+                                'Unnamed: 0_y',
+                                'airport_name',
+                                'airport'], axis=1)
+    return merged_df
