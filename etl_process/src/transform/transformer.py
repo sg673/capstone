6f4642f7915775a60fee6b8d52da0db043502739
sg673 <sg673@student.le.ac.uk>
2025-08-18 12:23:05 +0100

    feat: added the transformer class to remove repitition in transform code

---
+++ etl_process/src/transform/transformer.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from src.utils.logging_utils import setup_logger
+import logging
+
+logger = setup_logger(__name__, "extract_data.log", level=logging.DEBUG)
+
+
+class Transformer:
+    def __init__(self, data: pd.DataFrame,
+                 crit_cols: "list[str]",
+                 col_types: "dict[str,list[str]]"):
+        self.data = data
+        self.crit_cols = crit_cols
+        self.col_types = col_types
+
+    def clean(self):
+        self.remove_duplicates()
+        self.format_columns()
+        self.handle_nulls()
+        self.format_data_types()
+        pass
+
+    def remove_duplicates(self):
+        duplicated_rows = self.data.duplicated()
+        self.clean_data = self.data[~duplicated_rows]
+        logger.info(f"Removed {duplicated_rows.sum()} duplicate rows")
+
+    def format_columns(self):
+        self.clean_data.columns = self.clean_data \
+                                  .columns.str \
+                                  .lower().str.replace(" ", "_")
+        logger.info("Column names formatted")
+
+    def handle_nulls(self):
+        rows_before_drop = len(self.clean_data)
+        self.clean_data = self.clean_data.dropna(subset=self.crit_cols)
+        rows_dropped = rows_before_drop - len(self.clean_data)
+
+        rows_with_nulls = self.clean_data.isna().any(axis=1).sum()
+        nulls_before_fill = self.clean_data.isna().sum().sum()
+        self.clean_data = self.clean_data.fillna(0)
+        nulls_after_fill = self.clean_data.isna().sum().sum()
+
+        if nulls_after_fill == 0:
+            logger.info(f"Dropped {rows_dropped} rows with critical nulls, "
+                        f"filled "
+                        f"{nulls_before_fill} nulls in {rows_with_nulls} rows")
+        else:
+            logger.warning(f"could not handle {nulls_after_fill} nulls")
+
+    def format_data_types(self):
+        type_mapping = {
+            "str_cols": "string",
+            "int_cols": "int64",
+            "float_cols": "float64"
+        }
+
+        for col_key, dtype in type_mapping.items():
+            cols = self.col_types[col_key]
+            self.clean_data[cols] = self.clean_data[cols].astype(dtype)
