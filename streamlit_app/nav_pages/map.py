25193df48c679a3aaa3f62c932ace5594e34e2fa
sg673 <sg673@student.le.ac.uk>
2025-08-20 12:17:39 +0100

    feat: added some styles to the generated map

--- streamlit_app/nav_pages/map.py
+++ streamlit_app/nav_pages/map.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 
 def map_display(data: pd.DataFrame):
@@ -13,4 +14,20 @@ def map_display(data: pd.DataFrame):
     state_summary['total_ct'] = state_summary[ct_cols].sum(axis=1)
     state_summary['arr_flights_pct'] = round(
         (state_summary['total_ct'] / state_summary['arr_flights']) * 100, 2)
-    st.map(data=state_summary)
+
+    fig = px.choropleth(
+        state_summary,
+        locations='state',
+        color='arr_flights_pct',
+        locationmode='USA-states',
+        scope='usa',
+        title='Flight Arrival Percentage by State'
+    )
+    fig.update_geos(projection_type="albers usa")
+    fig.update_layout(
+        margin=dict(l=20, r=20, t=20, b=20),
+        paper_bgcolor="#cfe0e8",
+        height=800
+    )
+    with st.container(key="us-map"):
+        st.plotly_chart(fig, height=800, on_select="ignore")
