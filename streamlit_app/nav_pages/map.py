e29c1901cdec26078830f3a3e72e877e3920281a
sg673 <sg673@student.le.ac.uk>
2025-08-20 13:33:34 +0100

    feat: added selection for delay type

--- streamlit_app/nav_pages/map.py
+++ streamlit_app/nav_pages/map.py
@@ -34,6 +34,11 @@ def map_display(data: pd.DataFrame):
         (state_summary['total_ct'] / state_summary['arr_flights']) * 100, 2)
     state_summary['state_name'] = state_summary['state'].map(state_names)
 
+    for col in ct_cols:
+        pct_col = col.replace('_ct', '_pct_of_delays')
+        state_summary[pct_col] = round(
+            (state_summary[col] / state_summary['total_ct']) * 100, 2)
+
     with st.container(key="us-map"):
         cols = st.columns(10)
         with cols[0]:
@@ -43,19 +48,33 @@ def map_display(data: pd.DataFrame):
                                          sorted(state_summary['year']
                                                 .unique()),
                                          label_visibility="collapsed")
-            filtered_data = state_summary[
-                state_summary['year'] == selected_year]
+        with cols[2]:
+            st.write("Delay Type")
+        with cols[3]:
+            delay_options = ['All Delays'] + [col.replace("_ct","").title()
+                                              for col in ct_cols]
+            selected_delay = st.selectbox("delay type",delay_options,
+                                          label_visibility="collapsed")
+
+        filtered_data = state_summary[state_summary['year'] == selected_year]
+
+        if selected_delay == 'All Delays':
+            color_col = "arr_flights_pct"
+            title_delay = "All Late Arrivals"
+        else:
+            color_col = f"{selected_delay.lower()}_pct_of_delays"
+            title_delay = f"{selected_delay} Delays"
 
         fig = px.choropleth(
             filtered_data,
             locations='state',
-            color='arr_flights_pct',
+            color=color_col,
             locationmode='USA-states',
             scope='usa',
-            title=f'Flight Arrival Percentage by State - {selected_year}',
-            labels={'arr_flights_pct': 'Late Arrival Percentage (%)'},
-            range_color=[state_summary['arr_flights_pct'].min(),
-                         state_summary['arr_flights_pct'].max()],
+            title=f'{title_delay} Percentage by State - {selected_year}',
+            labels={color_col: f'{title_delay} Percentage (%)'},
+            range_color=[state_summary[color_col].min(),
+                         state_summary[color_col].max()],
             hover_name='state_name'
         )
         fig.update_geos(projection_type="albers usa")
