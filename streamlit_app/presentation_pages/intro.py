725f90e9f8c669dff959ddc0e201f1cc0b7def6b
sg673 <sg673@student.le.ac.uk>
2025-08-21 11:05:05 +0100

    feat: added intro page

--- streamlit_app/presentation_pages/intro.py
+++ streamlit_app/presentation_pages/intro.py
@@ -1,2 +1,7 @@
+import streamlit as st
+
+
 def pres_intro():
-    pass
+    # HTML file is largely AI generated
+    with open("presentation_pages/intro.html") as f:
+        st.markdown(f.read(), unsafe_allow_html=True)
