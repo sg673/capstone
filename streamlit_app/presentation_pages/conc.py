c454debf3ff17e28d5d3185c599b15eb782ccaa8
sg673 <sg673@student.le.ac.uk>
2025-08-21 14:52:55 +0100

    feat: added conclusion page

--- streamlit_app/presentation_pages/conc.py
+++ streamlit_app/presentation_pages/conc.py
@@ -1,2 +1,7 @@
+import streamlit as st
+
+
 def pres_conc():
-    pass
+    # HTML file is largely AI generated
+    with open("presentation_pages/conclusion.html") as f:
+        st.markdown(f.read(), unsafe_allow_html=True)
